a6c3be6a2bde60b4e81e4f20c4db656a5f71a3db
itsimplis <i.tsimplis@gmail.com>
2023-11-07 16:44:21 +0200

    re-designed database, better, with constraints

--- server/database/create_db_schema.sql
+++ server/database/create_db_schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS p.consumption;
 DROP TABLE IF EXISTS p.device;
 DROP TABLE IF EXISTS p.user;
-DROP TABLE IF EXISTS p.user_consumption;
+DROP TABLE IF EXISTS p.device_consumption;
+DROP TABLE IF EXISTS p.alerts;
 DROP SCHEMA IF EXISTS p;
 
 CREATE SCHEMA p;
@@ -24,30 +25,34 @@ CREATE TABLE p.user (
     password VARCHAR(100),
 	first_name VARCHAR(100),
 	last_name VARCHAR(100),
-	age VARCHAR(100),
-	gender VARCHAR(100),
-	country VARCHAR(100),
-    visibility VARCHAR(100),
-    notifications VARCHAR(100)
+	age VARCHAR(3),
+	gender VARCHAR(10),
+	country VARCHAR(50),
+    visibility VARCHAR(10),
+    notifications VARCHAR(3)
 );
 
-CREATE TABLE p.user_consumption (
-    user_username VARCHAR(50) NOT NULL,
-    consumption_id INTEGER NOT NULL,
-    PRIMARY KEY(user_username, consumption_id)
+CREATE TABLE p.device (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_username VARCHAR(100) NOT NULL REFERENCES p.user(username),
+    device_type VARCHAR(100),
+    device_category VARCHAR(50),
+    device_name TEXT
+);
+
+CREATE TABLE p.device_consumption (
+    id SERIAL PRIMARY KEY NOT NULL,
+    device_id INT NOT NULL REFERENCES p.device(id),
+    consumption_id INT NOT NULL REFERENCES p.consumption(id)
 );
 
 CREATE TABLE p.alert (
     id SERIAL PRIMARY KEY NOT NULL,
-    username VARCHAR(100),
+    username VARCHAR(100) REFERENCES p.user(username),
+    device_id INT REFERENCES p.device(id),
     title VARCHAR(100),
     description VARCHAR(250),
-    date TEXT,
+    date TIMESTAMP WITH TIME ZONE,
     type CHAR(1),
     read_status CHAR(1)
-);
-
-CREATE TABLE p.device (
-    type SERIAL PRIMARY KEY NOT NULL,
-    power_per_second DECIMAL(6,1)
 );
