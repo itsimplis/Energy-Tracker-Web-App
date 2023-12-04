d2bcc96153952889047b85e5c0210af1d34695d7
itsimplis <i.tsimplis@gmail.com>
2023-12-04 18:28:21 +0200

    testing power distribution chart variations

--- app/src/app/pages/dashboard/dashboard.component.ts
+++ app/src/app/pages/dashboard/dashboard.component.ts
@@ -12,6 +12,50 @@ export class DashboardComponent implements OnInit {
 
   counters: any[];
   totalPower: any[];
+  distribution: any[] = [
+    {
+      "name": "Germany",
+      "value": 40632,
+      "extra": {
+        "code": "de"
+      }
+    },
+    {
+      "name": "United States",
+      "value": 50000,
+      "extra": {
+        "code": "us"
+      }
+    },
+    {
+      "name": "France",
+      "value": 36745,
+      "extra": {
+        "code": "fr"
+      }
+    },
+    {
+      "name": "United Kingdom",
+      "value": 36240,
+      "extra": {
+        "code": "uk"
+      }
+    },
+    {
+      "name": "Spain",
+      "value": 33000,
+      "extra": {
+        "code": "es"
+      }
+    },
+    {
+      "name": "Italy",
+      "value": 35800,
+      "extra": {
+        "code": "it"
+      }
+    }
+  ]
   totalPowerGrouped: any[];
   highestReadings: any[];
   lowestReadings: any[];
