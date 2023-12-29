3670739eedc193e3f3a99616232848e5d7b84906
itsimplis <i.tsimplis@gmail.com>
2023-12-29 22:28:30 +0200

    Updated '/getTotalPowerPerDevice' endpoint values from Watt to kWh

--- server/routers/data/api.py
+++ server/routers/data/api.py
@@ -613,7 +613,7 @@ async def get_total_power_per_device(username: str = Depends(get_current_user)):
         try:
             keys = ["device_id", "device_name", "device_category", "device_type", "total_power"]
             result = connector.execute("""
-                SELECT p.device.id, p.device.device_name, p.device.device_category, p.device.device_type, COALESCE(SUM(p.power_reading.power), 0) AS total_power
+                SELECT p.device.id, p.device.device_name, p.device.device_category, p.device.device_type, COALESCE(SUM(p.power_reading.power), 0) / 1000 AS total_power
                 FROM p.device
                 LEFT JOIN p.device_consumption ON p.device.id = p.device_consumption.device_id
                 LEFT JOIN p.power_reading ON p.device_consumption.consumption_id = p.power_reading.consumption_id
